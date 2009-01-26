013b81209da49250443fea73cc7cbda596783f31
Kevin Ball <kball@kevin-balls-macbook-pro.local>
2009-01-25 18:56:59 -0800

    Update demo app to be more interesting

--- demo.rb
+++ demo.rb
@@ -2,15 +2,22 @@ load 'shoe_query.rb'
 app = Shoes.app do
   para "foo"
   f = flow do
+    para "bar"
     stack do 
-      para "bar"
       para 'blah'
     end
   end
-  b = button "click me"
+  b = button "click me to change all paragraphs"
   b.click do
-    para shoe_query('para').inspect
-    para shoe_query('flow para').inspect
-    para shoe_query(f).find('para').inspect
+    shoe_query('para').each {|p| p.text = "all ps" }
   end
+  b2 = button "click me to just change the paragraphs inside the flow"
+  b2.click do
+    shoe_query('flow').find('para').each {|p| p.text = "flow ps"}
+  end
+  b3 = button "click me to change the button inside the stack"
+  b3.click do
+    shoe_query('stack para').each {|p| p.text = "stack p"}
+  end
+
 end
